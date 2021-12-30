9d5cc5d8972259bc3f23aec8ed12b694d5c5d37f
hossamelgendy1 <59685428+hossamelgendy1@users.noreply.github.com>
2021-12-30 21:08:57 +0200

    Initial commit

---
+++ src/main/java/SWProject/SweiiProjectApplication.java
@@ -0,0 +1,13 @@
+package SWProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweiiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweiiProjectApplication.class, args);
+	}
+
+}
